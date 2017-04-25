935842a5fc748762084e2cd8d1cf90f87e1173ee
ben-sheeran <benjamin_sheeran@brown.edu>
2017-04-25 01:36:10 -0400

    perf

--- falling/falling.js
+++ falling/falling.js
@@ -1,63 +1,74 @@
-var song;
+var envelope, fft;
 
-function preload() {
-  song = loadSound('sounds/drone.ogg');
-}
+var currFreq;
 
-var NUMSINES = 5; // how many of these things can we do at once?
-var sines = new Array(NUMSINES); // an array to hold all the current angles
-var rad; // an initial radius value for the central sine
-var i; // a counter variable
+var oscArray = [new p5.SinOsc(),new p5.SinOsc(),new p5.SinOsc(),new p5.SinOsc(),new p5.SinOsc()];
 
-// play with these to get a sense of what's going on:
-var fund = 0.005; // the speed of the central sine
-var ratio = .1; // what multiplier for speed is each additional sine?
-var alpha = 50; // how opaque is the tracing system
+var major = [0,4,7,11,14];
+var dim = [0,3,6,9,13];
+var minor = [0,3,7,10,14];
+var seven = [0,4,7,10,14];
+var majsix = [0,4,7,9,21];
 
-var trace = false; // are we tracing?
+var c = 60;
+var dflat = 61;
+var bflat = 58;
+var aflat = 56;
+var eflat = 63;
+var f = 65;
+var gflat = 66;
+
+//12 chords
+var progBase = [dflat,c,bflat,aflat,eflat,aflat,eflat,f,bflat,gflat,bflat,aflat];
+var progTypes  = [major,dim,minor,major,minor,major,minor,major,minor,majsix,seven,seven];
+var chordIndex;
+
+var oscIndex;
 
 function setup() {
-  createCanvas(1000, 1000);
-	song.loop();
-  rad = height/4; // compute radius for central circle
-  background(204); // clear the screen
+  createCanvas(710, 200);
 
-  for (var i = 0; i<sines.length; i++) {
-    sines[i] = PI; // start EVERYBODY facing NORTH
-  }
+  // Instantiate the envelope
+  envelope = new p5.Env();
+
+  // set attackTime, decayTime, sustainRatio, releaseTime
+  envelope.setADSR(0.001, 0.3, 0.1, 0.3);
+
+  // set attackLevel, releaseLevel
+  envelope.setRange(1, 0);
+
+  oscIndex = 0;  
+  chordIndex = 0;
+
+  currFreq = 20;
+
+  fft = new p5.FFT();
+  noStroke();
 }
 
-function draw() {
-  if (!trace) {
-    background(204); // clear screen if showing geometry
-    stroke(0, 255); // black pen
-    noFill(); // don't fill
-  } 
-
-  // MAIN ACTION
-  push(); // start a transformation matrix
-  translate(width/2, height/2); // move to middle of screen
-
-  for (var i = 0; i<sines.length; i++) {
-    var erad = 0; // radius for small "point" within circle... this is the 'pen' when tracing
-    // setup for tracing
-    if (trace) {
-      stroke(0, 0, 255*(float(i)/sines.length), alpha); // blue
-      fill(0, 0, 255, alpha/2); // also, um, blue
-      erad = 5.0*(1.0-float(i)/sines.length); // pen width will be related to which sine
-    }
-    var radius = rad/(i+1); // radius for circle itself
-    rotate(sines[i]); // rotate circle
-    if (!trace) ellipse(0, 0, radius*2, radius*2); // if we're simulating, draw the sine
-    push(); // go up one level
-    translate(0, radius); // move to sine edge
-    if (!trace) ellipse(0, 0, 5, 5); // draw a little circle
-    if (trace) ellipse(0, 0, erad, erad); // draw with erad if tracing
-    pop(); // go down one level
-    translate(0, radius); // move into position for next sine
-    sines[i] = (sines[i]+(fund+(fund*i*ratio)))%TWO_PI; // update angle based on fundamental
+function keyTyped() {
+  if(key === ' ') {
+    chordIndex = (chordIndex + 1) %12;
+    return;
   }
-  
-  pop(); // pop down final transformation
-  
+  if(keyCode === ENTER) {
+    oscArray[0].stop();
+    oscArray[1].stop();
+    oscArray[2].stop();
+    oscArray[3].stop();
+    oscArray[4].stop();
+  }
+  var note = Math.floor(Math.random() * 5);
+  var base = progBase[chordIndex];
+  var ch = progTypes[chordIndex];
+  var midiValue = base + ch[note] - 12 + 12 * Math.floor(Math.random() * 3);
+  var freqValue = midiToFreq(midiValue);
+  currFreq += freqValue;
+  var osc = oscArray[oscIndex]
+  osc.freq(freqValue);
+  osc.start();
+
+  oscIndex = (oscIndex + 1)%5;
+  oscArray[oscIndex].stop();
 }
+
